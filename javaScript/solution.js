c8c9aa408c303dae714dea8e5119b22c5ea107c4
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-03-27 23:30:24 +0300

    Create solution.js

---
+++ javaScript/solution.js
@@ -0,0 +1,14 @@
+ /**** Soccer points *****/ 
+//Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+function footballPoints(a, b, c) {
+    if (isNaN(a) || isNaN(b) || isNaN(b)) {
+      return "Please provide numerical values";
+    }
+    let winPoints = a * 3;
+    let drawPoints = b;
+    let total = winPoints + drawPoints;
+    return total;
+  }
+
+  let points = footballPoints(11, 4, 6);
+  console.log(points);
