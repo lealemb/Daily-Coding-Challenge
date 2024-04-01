4035c06440ed239783c394cff688ef62d8503207
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-01 23:36:12 +0300

    Return Something to Me!

---
+++ javaScript/returnSth.js
@@ -0,0 +1,18 @@
+/**
+Return Something to Me!
+
+Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+Examples
+giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+giveMeSomething("something") ➞ "something something"
+**/
+
+function giveMeSomething(a) {
+	let b = "something";
+	return b + " " + a;
+	
+}
