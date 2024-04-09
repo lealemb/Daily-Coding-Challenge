75abc2f7cde639e4cbe60b83daf0ef957655e7ef
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-09 20:17:06 +0300

    DetectTheWord

---
+++ javaScript/DetectTheWord.js
@@ -0,0 +1,12 @@
+//detect the word from the give string or input strings upporcase from lower and write that detected word in one variable
+function findHiddenWord(a){
+    let hiddenWordContainer = "";
+    for(let i = 0; i < a.length; i++){
+        let singleLetter = a.charAt(i);
+        if (singleLetter !== singleLetter.toUpperCase()){
+            hiddenWordContainer += singleLetter;
+        }
+    }
+    return hiddenWordContainer;
+}
+console.log(findHiddenWord("bEFGBu"));
