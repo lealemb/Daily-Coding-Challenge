a9bce4a58b36756b7059f82ad3abee39bee55689
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-09 20:26:10 +0300

    DetectTheWord

--- javaScript/DetectTheWord.js
+++ javaScript/DetectTheWord.js
@@ -1,4 +1,4 @@
-//detect the word from the give string or input strings upporcase from lower and write that detected word in one variable
+//detect the word from the give string or input strings upporcase from lower and write that detected word in one variable.
 function findHiddenWord(a){
     let hiddenWordContainer = "";
     for(let i = 0; i < a.length; i++){
