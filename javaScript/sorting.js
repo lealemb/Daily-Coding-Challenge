c30b2deda65058aeca30db8c09c41d38da8c2a50
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-15 22:49:56 +0300

    sorting

---
+++ javaScript/sorting.js
@@ -0,0 +1,23 @@
+function sortNumbersInarray(numbers){
+
+    let j =true;
+
+    while (j) {
+        j = false;
+        for (let i = 0; i < numbers.length-1;i++)
+        {
+            if (numbers[i] > numbers[i+1]){
+                j = true;
+                let temp = numbers[i];
+                numbers[i] = numbers[i+1];
+                numbers[i+1] = temp;
+                
+            }
+
+        }
+}
+    return numbers;
+
+}
+
+console.log(sortNumbersInarray([5,8,113,9,12,56,88]));
