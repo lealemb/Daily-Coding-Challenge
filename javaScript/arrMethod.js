6ccf657608daa6d49d98bcdcd3b654379058457f
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-18 21:04:07 +0300

    Create arrMethod.js

---
+++ javaScript/arrMethod.js
@@ -0,0 +1,10 @@
+//For example, we have an array of users, each with the fields id and name. Let’s find the one with id == 1:
+let users = [
+  {id: 1, name: "John"},
+  {id: 2, name: "Pete"},
+  {id: 3, name: "Mary"}
+];
+
+let user = users.find(item => item.id == 1);
+
+alert(user.name); // John
