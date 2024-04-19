ba8f725e56b1c31bb5ede6b917aa919c4f18471f
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-19 23:39:54 +0300

    concatMethod

--- javaScript/concatMethod.js
+++ javaScript/concatMethod.js
@@ -1,6 +1,8 @@
-//concat( )
-//This method is used to merge two or more arrays and returns a new array, without changing the existing arrays.
+/**
+concat( )
+This method is used to merge two or more arrays and returns a new array, without changing the existing arrays.
 
+**/
 const arr1 = ["a", "b", "c"]; 
 
 const arr2 = ["d", "e", "f"];
