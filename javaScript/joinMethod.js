3d389625e7ed21cf8be40bf6ad626430116398cc
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-19 23:38:58 +0300

    joinMethod

---
+++ javaScript/joinMethod.js
@@ -0,0 +1,8 @@
+/**
+join( )
+This method returns a new string by concatenating all of the array’s
+elements separated by the specified separator.
+**/
+
+const arr = ["m", "a", "n", "d", "e", "e", "p"]; 
+console.log(arr.join('')); // mandeep
