e2381b8297528dbe108d80a179f2a11a6b1646b9
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-20 19:45:26 +0300

    Create SpliceArrMethod.js

---
+++ javaScript/SpliceArrMethod.js
@@ -0,0 +1,8 @@
+let arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr.splice(2, 0, "complex", "language");
+
+alert( arr ); // "I", "study", "complex", "language", "JavaScript"
