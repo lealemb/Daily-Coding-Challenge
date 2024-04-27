063fa43526ce3e622740f460c755497522ab18e7
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-04-27 22:49:34 +0300

    Create shiftMethod.js

---
+++ javaScript/shiftMethod.js
@@ -0,0 +1,6 @@
+//Let’s now create a new array of animal names and then learn how to use the shift() method to remove the first name from the array:
+
+let animalNames = ["Lion", "Dog", "Snake", "Tiger"];
+animalNames.shift(); // "Lion"
+
+console.log(animalNames); // ['Dog', 'Snake', 'Tiger']
